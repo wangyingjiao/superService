384f3b5b062513715b0d357a5f8dda53d3070c88
漫长 <15660061100@163.com>
2017-11-28 18:22:33 +0800

    技能

--- src/api/skill.js
+++ src/api/skill.js
@@ -4,15 +4,24 @@ const instance = axios.create({
   headers: { 'content-type': 'application/json;charset=UTF-8' }
 
 })
-// 提交
-// export function addTech(obj) {
-//   return instance.post(`/api/a/service/technician/serviceTechnicianInfo/savePersonalData`,obj)
-// }
+// 服务站Talbe
+export function Skillserver(obj) {
+  return instance.post(`/api/a/service/skill/serSkillInfo/choiceTechnicianlistData`,obj)
+}
 //获取列表
 export function getListdata() {
     return instance.get(`/api/a/service/skill/serSkillInfo/listData`)
 }
-// //获取省级区域
-// export function getArea() {
-//     return instance.get(`/api/a/sys/area/getchildArea`)
-// }
+//获取服务列表
+export function getListser() {
+  return instance.get(`/api/a/service/skill/serSkillInfo/getServiceStationList`)
+}
+
+export function orderServer(obj) {
+  return instance.post(`/api/a/service/skill/serSkillInfo/choiceSerlistData`,obj)
+}
+//删除技能
+
+export function techDelet() {
+  return instance.post(`/api/a/service/skill/serSkillInfo/deleteSortInfo`,obj)
+}
