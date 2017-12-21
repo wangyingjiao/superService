e524e82995f2c8534dba4d92dd93603dd3ce61e6
yuwuwu <yyxpublic@163.com>
2017-12-21 18:49:41 +0800

    调整图片上传

--- src/api/sign.js
+++ src/api/sign.js
@@ -14,12 +14,7 @@ export function getSign() {
     instance.get(`/api/oss/getSign`).then(res => {
       console.log(res.data)
       var obj = res.data
-      // var expiresDate = new Date()
-      // expiresDate.setTime(expiresDate.getTime() + (30 * 60 * 1000))
-      // console.log(expiresDate)
-      // Cookies.set('sign', JSON.stringify(obj), { expires: expiresDate })
       Cookies.set('sign', JSON.stringify(obj), { expires: 0.35 })
-      // console.log(JSON.parse(Cookies.get('sign')))
       sign = JSON.parse(Cookies.get('sign'))
     })
   }
