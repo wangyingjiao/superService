c203a1df8f9cddf6b7cfc88514bd5102a71786ab
yuwuwu <yyxpublic@163.com>
2017-12-26 17:55:31 +0800

    11

--- config/index.js
+++ config/index.js
@@ -44,7 +44,7 @@ module.exports = {
        // }
      // }
       '/api': {
-        target: 'http://10.16.70.130:8800/',
+        target: 'http://10.16.70.136:8800/',
         changeOrigin: false, // 如果接口跨域，需要进行这个参数配置
         pathRewrite: {
           '^/api': ''
