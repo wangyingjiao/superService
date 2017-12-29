2a1181b9605eb9310b20010097f117bc5b321033
yuwuwu <yyxpublic@163.com>
2017-12-29 17:23:36 +0800

    解决冲突

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
   NODE_ENV: '"production"'
-=======
-  NODE_ENV: '"production"',
-  BASE_API: '"https://apiservice.guoanshequ.wang/"',
->>>>>>> 148b556c07c378437525c4a78f49488c0a366a4e
 }
