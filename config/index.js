7a785e0aa2be9bdf48f15d37a192e56edf3406a6
yuwuwu <yyxpublic@163.com>
2018-01-06 10:04:11 +0800

    修改冲突

--- config/index.js
+++ config/index.js
@@ -43,23 +43,13 @@ module.exports = {
         //  '^/api': ''
        // }
      // }
-<<<<<<< HEAD
-      // '/apiservice': {
-      //   target: 'http://10.16.70.133:8800/',
-      //   changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
-      //   pathRewrite: {
-      //     '^/apiservice': ''
-      //   }
-      // }
-=======
       '/apiservice': {
-        target: 'http://10.16.70.130:8800/',
+        target: 'http://10.16.70.133:8800/',
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         pathRewrite: {
           '^/apiservice': ''
         }
       }
->>>>>>> e611d6db4cdc7266706172f5d7ff5c5524e8b304
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
