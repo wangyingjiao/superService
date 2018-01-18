a7de34ce94d8b5101b6e8045487dfaa2300afc05
yuyongxing <yyxpublic@163.com>
2018-01-18 09:30:42 +0800

    权限bug

--- src/views/staff/postManagement.vue
+++ src/views/staff/postManagement.vue
@@ -523,27 +523,27 @@ export default {
       if (b) {
         console.log('tttttttttttttttt')
         // 处理订单里的查看详情
-        // if (
-        //   ["order_time", "order_dispatch", "order_addTech"].indexOf(
-        //     a.permission
-        //   ) > -1
-        // ) {
+        if (
+          ["order_time", "order_dispatch", "order_addTech"].indexOf(
+            a.permission
+          ) > -1
+        ) {
   
-        //   var arr = a.parentIds.split(",");
-        //    for (var i = 0; i < this.data2.length; i++) {
-        //      if (this.data2[i].subMenus != undefined) {
-        //        for (var j = 0; j < this.data2[i].subMenus.length; j++) {
+          var arr = a.parentIds.split(",");
+           for (var i = 0; i < this.data2.length; i++) {
+             if (this.data2[i].subMenus != undefined) {
+               for (var j = 0; j < this.data2[i].subMenus.length; j++) {
                 
-        //         if (this.data2[i].subMenus[j].permission == "order") {
-        //           console.log(this.data2[i].subMenus[j],'成功')
-        //           this.$refs.domTree.setChecked(this.data2[i].subMenus[j].subMenus[1].id, true);
-        //         }
-        //        }
-        //      }else{
-        //        console.log(this.data2[i].subMenus)
-        //      }
-        //    }
-        // }
+                if (this.data2[i].subMenus[j].permission == "order") {
+                  console.log(this.data2[i].subMenus[j],'成功')
+                  this.$refs.domTree.setChecked(this.data2[i].subMenus[j].subMenus[this.data2[i].subMenus[j].subMenus.length-2].id, true);
+                }
+               }
+             }else{
+               console.log(this.data2[i].subMenus)
+             }
+           }
+        }
         //订单详情处理完毕
         //自动勾选列表权限
         if (a.subMenus == undefined) {
@@ -587,12 +587,12 @@ export default {
                 if (this.data2[i].subMenus[j].permission == "order") {
                   console.log(a.permission,'2',this.temp.check)
                   var orderarr = this.data2[i].subMenus[j]
-                  for(var k = 2;k<orderarr.subMenus.length;k++){
+                  for(var k = 2;k<orderarr.subMenus.length-1;k++){
                     //console.log('不可取消')
                        if(this.temp.check.indexOf(orderarr.subMenus[k].id)>-1){
                          console.log(a.permission,'3')
                          console.log(this.data2[i].subMenus[j].subMenus[1].name,'详情权限iiiii')
-                         this.$refs.domTree.setChecked(this.data2[i].subMenus[j].subMenus[1].id, true);
+                         this.$refs.domTree.setChecked(this.data2[i].subMenus[j].subMenus[orderarr.subMenus.length-2].id, true);
                          this.temp.check = this.$refs.domTree.getCheckedKeys();
                        }
                   }
