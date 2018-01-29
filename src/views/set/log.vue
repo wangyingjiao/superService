5de147930613653e809270d869564ca4deb86919
yuyongxing <yyxpublic@163.com>
2018-01-29 09:33:25 +0800

    字段名

--- src/views/set/log.vue
+++ src/views/set/log.vue
@@ -49,19 +49,19 @@
       <el-table-column align="center" label="创建时间" prop="createDate">      
       </el-table-column>
 
-      <el-table-column align="center" label="IP" prop="remoteAddr">      
+      <el-table-column align="center" label="操作IP地址" prop="remoteAddr">      
       </el-table-column>
 
-      <el-table-column align="center" label="title" prop="title">      
+      <el-table-column align="center" label="日志标题" prop="title">      
       </el-table-column>
 
-      <el-table-column align="center" label="分类" prop="type">      
+      <el-table-column align="center" label="日志类型" prop="type">      
       </el-table-column>
 
-      <el-table-column align="center" label="exceptions" prop="exceptions">      
+      <el-table-column align="center" label="异常信息" prop="exceptions">      
       </el-table-column>
 
-      <el-table-column align="center" width="150px" label="params" prop="params">    
+      <el-table-column align="center" width="150px" label="提交数据" prop="params">    
           <template scope="scope">
            <el-tooltip placement="left" :disabled="scope.row.params.length < 10" :content="scope.row.params">
              <div class="tool" >{{scope.row.params}}</div>
