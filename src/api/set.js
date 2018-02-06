36134df26c86bcbab461aaf16e438f615c3bde82
yuyongxing <yyxpublic@163.com>
2018-02-06 13:39:31 +0800

    字典表

--- src/api/set.js
+++ src/api/set.js
@@ -59,3 +59,11 @@ export function getPushMes(obj, pageNumber, pageSize) {
 export function sendPushMes(obj) {
   return instance.post(`/apiservice/a/sys/pushMessage/pushFailMessage`, obj)
 }
+// 获取字典列表
+export function getDict(obj, pageNumber, pageSize) {
+  return instance.post(`/apiservice/a/sys/dict/dictListData?pageNo=` + pageNumber + '&pageSize=' + pageSize, obj)
+}
+// 查看字典详情
+export function readDict(obj) {
+  return instance.post(`/apiservice/a/sys/dict/dictListDataByType`, obj)
+}
