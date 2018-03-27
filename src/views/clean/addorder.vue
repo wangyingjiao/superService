984d9a650fb3541a894636b1708eba860562f224
wangyuelei <lshmilyily@sohu.com>
2018-03-27 16:55:20 +0800

    dd

--- src/views/clean/addorder.vue
+++ src/views/clean/addorder.vue
@@ -579,7 +579,8 @@ export default {
       addAddrssDialogShow: false,
       tableDataAddress: [
         {
-          id: "ab",         
+          id: "ab",
+                   
           name: "jack",
           phone: "13426345690",
           address: "天津天津市和平区1222222",
