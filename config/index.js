c1e3f68d550a943307dcc00b0bb095e907beabd5
caojuwei <caojuwei123@163.com>
2018-04-11 10:50:12 +0800

    面包屑tab bug

--- config/index.js
+++ config/index.js
@@ -28,14 +28,14 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-    //  '/apiservice': {
-    //     'target': 'https://superservice.guoanshequ.wang/',
-    //     'secure': false, 
-    //     'changeOrigin': true, // 如果接口跨域，需要进行这个参数配置
-    //     'pathRewrite': {
-    //       '^/apiservice': '/apiservice'
-    //     }
-    //   },
+     '/apiservice': {
+        'target': 'https://superservice.guoanshequ.wang/',
+        'secure': false, 
+        'changeOrigin': true, // 如果接口跨域，需要进行这个参数配置
+        'pathRewrite': {
+          '^/apiservice': '/apiservice'
+        }
+      },
      // '/api': {
        // target: 'http://www.localhost.8800/',
        // changeOrigin: false, // 如果接口跨域，需要进行这个参数配置
@@ -50,13 +50,13 @@ module.exports = {
       //     '^/apiservice': ''
       //   }
       // }
-      '/apiservice': {
-        target: 'http://10.16.70.133:8800/',
-        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
-        pathRewrite: {
-          '^/apiservice': ''
-        }
-      }
+      // '/apiservice': {
+      //   target: 'http://10.16.70.133:8800/',
+      //   changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+      //   pathRewrite: {
+      //     '^/apiservice': ''
+      //   }
+      // }
      },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
--- src/permission.js
+++ src/permission.js
@@ -8,7 +8,7 @@ import { getToken } from '@/utils/auth' // 验权
 const whiteList = ['/login', '/download'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
   // NProgress.start()
-  if (getToken()) {
+  if (getToken() && localStorage.getItem('name')) {
     if (to.path === '/login') {
       next({ path: '/' })
     } else {
