f81f711a27f2b78b9bfdccc3d5c635ec3c82a2d2
yuyongxing <yyxpublic@163.com>
2018-04-20 18:03:01 +0800

    生产

--- config/index.js
+++ config/index.js
@@ -34,6 +34,7 @@ module.exports = {
         'changeOrigin': true, // 如果接口跨域，需要进行这个参数配置
         'pathRewrite': {
           '^/apiservicejs': '/apiservicejs'
+          
         }
       },
      // '/api': {
