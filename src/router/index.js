ba7c958d7661aae91f878f5c083722004c53b002
yuyongxing <yyxpublic@163.com>
2018-05-03 15:48:54 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -9,15 +9,15 @@ import Layout from '../views/layout/Layout'
 Vue.use(Router)
 
 export const constantRouterMap = [
-  { path: '/login', component: _import('login/index'), meta: { keepAlive: true }},
-  { path: '/download', component: _import('login/download'), meta: { keepAlive: true }},
+  { path: '/login', component: _import('login/index'), meta: { keepAlive: false }},
+  { path: '/download', component: _import('login/download'), meta: { keepAlive: false }},
   { path: '/404', component: _import('404') },
   {
     path: '/',
     component: Layout,
     redirect: '/dashboard',
     name: 'Dashboard',
-    children: [{ path: 'dashboard', meta: { keepAlive: true }, component: _import('dashboard/index') }]
+    children: [{ path: 'dashboard', meta: { keepAlive: false }, component: _import('dashboard/index') }]
   },
 
   {
@@ -25,17 +25,17 @@ export const constantRouterMap = [
     component: Layout,
     name: '员工管理',
     children: [
-      { path: 'user/index', meta: { keepAlive: true }, component: _import('staff/user'), name: '员工列表' },
-      { path: 'role/list', meta: { keepAlive: true }, component: _import('staff/role'), name: '岗位管理' }
+      { path: 'user/index', meta: { keepAlive: false }, component: _import('staff/user'), name: '员工列表' },
+      { path: 'role/list', meta: { keepAlive: false }, component: _import('staff/role'), name: '岗位管理' }
     ],
-    meta: { keepAlive: true }
+    meta: { keepAlive: false }
   },
   {
     path: '/basic',
     component: Layout,
     name: '基础服务',
     children: [
-      { path: 'organization', meta: { keepAlive: true }, name: '服务机构', component: _import('basic/organization') }
+      { path: 'organization', meta: { keepAlive: false }, name: '服务机构', component: _import('basic/organization') }
     ]
   },
   {
@@ -43,9 +43,9 @@ export const constantRouterMap = [
     component: Layout,
     name: '服务技师管理',
     children: [
-      { path: 'holiday', meta: { keepAlive: true }, name: '休假管理', component: _import('technician/holiday') },
-      { path: 'techni', meta: { keepAlive: true }, name: '技师管理', component: _import('technician/techni') },
-      { path: 'Schedule', meta: { keepAlive: true }, name: '排期管理', component: _import('technician/Schedule') }
+      { path: 'holiday', meta: { keepAlive: false }, name: '休假管理', component: _import('technician/holiday') },
+      { path: 'techni', meta: { keepAlive: false }, name: '技师管理', component: _import('technician/techni') },
+      { path: 'Schedule', meta: { keepAlive: false }, name: '排期管理', component: _import('technician/Schedule') }
     ]
   },
 
@@ -54,11 +54,11 @@ export const constantRouterMap = [
     component: Layout,
     name: '服务管理',
     children: [
-      { path: 'class', meta: { keepAlive: true }, name: '服务分类', component: _import('serviceManage/classify') },
-      { path: 'project', meta: { keepAlive: true }, name: '服务项目', component: _import('serviceManage/project') },
-      { path: 'skill', meta: { keepAlive: true }, name: '技能管理', component: _import('serviceManage/skill') },
-      { path: 'station', meta: { keepAlive: true }, name: '服务站', component: _import('basic/station') },
-      { path: 'buttDetails', meta: { keepAlive: true }, name: '对接详情', component: _import('serviceManage/buttDetails') }
+      { path: 'class', meta: { keepAlive: false }, name: '服务分类', component: _import('serviceManage/classify') },
+      { path: 'project', meta: { keepAlive: false }, name: '服务项目', component: _import('serviceManage/project') },
+      { path: 'skill', meta: { keepAlive: false }, name: '技能管理', component: _import('serviceManage/skill') },
+      { path: 'station', meta: { keepAlive: false }, name: '服务站', component: _import('basic/station') },
+      { path: 'buttDetails', meta: { keepAlive: false }, name: '对接详情', component: _import('serviceManage/buttDetails') }
     ]
   },
   {
@@ -67,13 +67,13 @@ export const constantRouterMap = [
     name: '保洁订单',
     children: [
       { path: 'ordermanage', name: '订单管理', component: _import('clean/ordermanage'), meta: { keepAlive: true }},
-      { path: 'addorder', meta: { keepAlive: true }, name: '新增订单', component: _import('clean/addorder') },
-      { path: 'dispatchmanage', meta: { keepAlive: true }, name: '工单管理', component: _import('clean/dispatchmanage') },
-      { path: 'returnvisit', meta: { keepAlive: true }, name: '回访记录', component: _import('clean/returnvisit') },
-      { path: 'orderinfo', meta: { keepAlive: true }, name: '订单详情', component: _import('clean/orderinfo') },
-      { path: 'dispatchReass', meta: { keepAlive: true }, name: '改派记录', component: _import('clean/dispatchReass') },
-      { path: 'refund', meta: { keepAlive: true }, name: '退款管理', component: _import('clean/refund') },
-      { path: 'pay', meta: { keepAlive: true }, name: '收款管理', component: _import('clean/pay') }
+      { path: 'addorder', meta: { keepAlive: false }, name: '新增订单', component: _import('clean/addorder') },
+      { path: 'dispatchmanage', meta: { keepAlive: false }, name: '工单管理', component: _import('clean/dispatchmanage') },
+      { path: 'returnvisit', meta: { keepAlive: false }, name: '回访记录', component: _import('clean/returnvisit') },
+      { path: 'orderinfo', meta: { keepAlive: false }, name: '订单详情', component: _import('clean/orderinfo') },
+      { path: 'dispatchReass', meta: { keepAlive: false }, name: '改派记录', component: _import('clean/dispatchReass') },
+      { path: 'refund', meta: { keepAlive: false }, name: '退款管理', component: _import('clean/refund') },
+      { path: 'pay', meta: { keepAlive: false }, name: '收款管理', component: _import('clean/pay') }
     ]
   },
   {
@@ -81,8 +81,8 @@ export const constantRouterMap = [
     component: Layout,
     name: ' 用户管理',
     children: [
-      { path: 'customermanage', meta: { keepAlive: true }, name: '用户管理', component: _import('customer/customermanage') },
-      { path: 'addcustomer', meta: { keepAlive: true }, name: '新增用户', component: _import('customer/addcustomer') }
+      { path: 'customermanage', meta: { keepAlive: false }, name: '用户管理', component: _import('customer/customermanage') },
+      { path: 'addcustomer', meta: { keepAlive: false }, name: '新增用户', component: _import('customer/addcustomer') }
     ]
   },
   {
@@ -90,14 +90,14 @@ export const constantRouterMap = [
     component: Layout,
     name: '系统管理',
     children: [
-      { path: 'log', meta: { keepAlive: true }, name: ' 日志管理', component: _import('set/log') },
-      { path: 'sysLog', meta: { keepAlive: true }, name: ' 对接日志管理', component: _import('set/sysLog') },
-      { path: 'appVersion', meta: { keepAlive: true }, name: ' APP发版', component: _import('set/appVersion') },
-      { path: 'menu', meta: { keepAlive: true }, name: ' 菜单管理', component: _import('set/menu') },
-      { path: 'dict', meta: { keepAlive: true }, name: ' 数据字典', component: _import('set/dict') },
-      { path: 'sysJointWait', meta: { keepAlive: true }, name: ' 待执行任务', component: _import('set/sysJointWait') },
-      { path: 'redis', meta: { keepAlive: true }, name: ' redis查询', component: _import('set/redis') },
-      { path: 'pushMes', meta: { keepAlive: true }, name: ' 推送消息', component: _import('set/pushMes') }
+      { path: 'log', meta: { keepAlive: false }, name: ' 日志管理', component: _import('set/log') },
+      { path: 'sysLog', meta: { keepAlive: false }, name: ' 对接日志管理', component: _import('set/sysLog') },
+      { path: 'appVersion', meta: { keepAlive: false }, name: ' APP发版', component: _import('set/appVersion') },
+      { path: 'menu', meta: { keepAlive: false }, name: ' 菜单管理', component: _import('set/menu') },
+      { path: 'dict', meta: { keepAlive: false }, name: ' 数据字典', component: _import('set/dict') },
+      { path: 'sysJointWait', meta: { keepAlive: false }, name: ' 待执行任务', component: _import('set/sysJointWait') },
+      { path: 'redis', meta: { keepAlive: false }, name: ' redis查询', component: _import('set/redis') },
+      { path: 'pushMes', meta: { keepAlive: false }, name: ' 推送消息', component: _import('set/pushMes') }
     ]
   },
 
