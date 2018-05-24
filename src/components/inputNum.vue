ce872ff080aa15a37452cb63943f2ef7617f89b1
yuyongxing <yyxpublic@163.com>
2018-05-24 14:20:52 +0800

    添加min,max参数

--- src/components/inputNum.vue
+++ src/components/inputNum.vue
@@ -1,10 +1,11 @@
 <template>
     <el-input
+    class="inputNum"
             disabled
             v-bind:value="value"
             v-on:change="$emit('input', $event.target.value)">
-            <el-button @click="minus" :disabled="value<=min?true:false" slot="prepend"><i class="el-icon-minus"></i></el-button>
-            <el-button @click="pius" :disabled="value>=max?true:false" slot="append"><i class="el-icon-plus"></i></el-button>
+            <el-button class="butMin" @click="minus" :disabled="value<=min?true:false" slot="prepend"><i class="el-icon-minus"></i></el-button>
+            <el-button class="butMax" @click="pius" :disabled="value>=max?true:false" slot="append"><i class="el-icon-plus"></i></el-button>
             </el-input>
 </template>
 <script>
@@ -27,5 +28,10 @@ export default {
   }
 };
 </script>
-<style>
+<style >
+
+.inputNum .el-input-group__append,.inputNum .el-input-group__prepend{
+  padding: 0 5px;
+}
+
 </style>
