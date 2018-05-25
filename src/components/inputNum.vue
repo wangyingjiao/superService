91ecc80faa6669eaf0098c6b42e68f03fb7c53a3
yuyongxing <yyxpublic@163.com>
2018-05-25 14:01:46 +0800

    修改没有参数时bug

--- src/components/inputNum.vue
+++ src/components/inputNum.vue
@@ -5,9 +5,9 @@
             v-bind:value="value"
             v-on:input="$emit('input', $event.target.value)"
             >
-            <el-button v-if="value<=min " class="butMin butDis" @click="dis" slot="prepend"><i class="el-icon-minus"></i></el-button>
-            <el-button v-if="value>min " class="butMin " @click="minus" slot="prepend"><i class="el-icon-minus"></i></el-button>
-            <el-button v-if="value<max " class="butMax" @click="pius"  slot="append"><i class="el-icon-plus"></i></el-button>
+             <el-button v-if="value<=min " class="butMin butDis" @click="dis" slot="prepend"><i class="el-icon-minus"></i></el-button>
+            <el-button v-if="value>min||!min" class="butMin " @click="minus" slot="prepend"><i class="el-icon-minus"></i></el-button>
+            <el-button v-if="value<max||!max" class="butMax" @click="pius"  slot="append"><i class="el-icon-plus"></i></el-button>
             <el-button v-if="max<=value" @click="dis" class="butMax butDis" slot="append"><i class="el-icon-plus"></i></el-button>
             </el-input>
 </template>
@@ -20,8 +20,8 @@ export default {
   props: ["text", "value", "min", "max", "change"],
   created() {},
   methods: {
-    dis(val){
-       console.log(val)
+    dis(val) {
+      console.log(val);
     },
     minus() {
       this.$emit("input", this.value - 1);
@@ -43,10 +43,10 @@ export default {
   padding: 0 5px;
   overflow: hidden;
 }
-.butDis{
-  background-color:#eef1f6 !important;
+.butDis {
+  background-color: #eef1f6 !important;
 }
-.butDis:hover{
-  background-color:#eef1f6 !important;
+.butDis:hover {
+  background-color: #eef1f6 !important;
 }
 </style>
